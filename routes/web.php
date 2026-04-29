2b992dcdbc1dddbb5e5baa08a7d84168290cda0c
Andrerrb <andrebatista2330@gmail.com>
2026-04-29 18:16:53 -0300

    Move API routes to api.php and configure routing separation

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PeopleController;
-use App\Http\Controllers\PlanetsController;
-
-Route::get('/people', [PeopleController::class, 'index']);
-Route::get('/people/{id}', [PeopleController::class, 'show']);
-Route::get('/planets',[PlanetsController::class, 'index']);
-Route::get('/planets/{id}',[PlanetsController::class, 'show']);
